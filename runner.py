e326708db7634c565db7e907bbb4f86b59882915
alishahwee <alisha.hwee@outlook.com>
2021-12-05 16:10:23 -0600

    Ensure proper parsing

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ def parse_puzzle_input(file: str) -> list[str]:
     """Parse the puzzle input so that each line is an element in a list.
     Then return the list."""
     with open(file) as f:
-        return f.read().splitlines()
+        return f.read().split("\n\n")
 
 
 if __name__ == "__main__":
--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-from types import Tuple
+from typing import Tuple
 
 
 def part_one(bingo_input: list[str]) -> int:
@@ -10,6 +10,9 @@ def part_two(bingo_input: list[str]) -> int:
 
 
 def parse_bingo_input(bingo_input: list[str]) -> Tuple[list[int], list[list[int]]]:
-    drawn_numbers = [int(num) for num in bingo_input[0]]
-    bingo_boards = [[int(num) for num in row.split()] for row in bingo_input[1:]]
+    drawn_numbers = [int(num) for num in bingo_input[0].split(",")]
+    bingo_boards = [
+        [[int(num) for num in row.split()] for row in board.split("\n")]
+        for board in bingo_input[1:]
+    ]
     return (drawn_numbers, bingo_boards)
