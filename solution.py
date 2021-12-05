b18c37e38e72aebf001b12f50ac687d7c3546751
alishahwee <alisha.hwee@outlook.com>
2021-12-05 16:35:13 -0600

    Fix types

--- solution.py
+++ solution.py
@@ -1,6 +1,3 @@
-from typing import Tuple
-
-
 def part_one(bingo_input: list[str]) -> int:
     pass
 
@@ -11,8 +8,8 @@ def part_two(bingo_input: list[str]) -> int:
 
 def parse_bingo_input(
     bingo_input: list[str],
-) -> Tuple[list[int], list[list[list[int]]]]:
-    drawn_numbers = set([int(num) for num in bingo_input[0].split(",")])
+) -> tuple[set[int], list[list[list[int]]]]:
+    drawn_numbers = {int(num) for num in bingo_input[0].split(",")}
     bingo_boards = [
         [[int(num) for num in row.split()] for row in board.split("\n")]
         for board in bingo_input[1:]
